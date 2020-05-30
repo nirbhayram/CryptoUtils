28fc555205b779acfbd38c08cbbdd2003557549d
Nirbhay <da201501068@gmail.com>
2020-05-30 13:29:30 +0530

    SymmetricEncryptionUtilsTest class added pom file changes for junit

---
+++ src/test/java/com/nirbhay/security/encryption/SymmetricEncryptionUtilsTest.java
@@ -0,0 +1,15 @@
+package com.nirbhay.security.encryption;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class SymmetricEncryptionUtilsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
