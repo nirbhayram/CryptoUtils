c9f66ba265bda603dad088de347430657d2fefc8
Nirbhay <da201501068@gmail.com>
2020-05-30 21:35:20 +0530

    SymmetricEncryptionUtils class developed.

--- src/main/java/com/nirbhay/security/encryption/SymmetricEncryptionUtils.java
+++ src/main/java/com/nirbhay/security/encryption/SymmetricEncryptionUtils.java
@@ -1,4 +1,84 @@
 package com.nirbhay.security.encryption;
 
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class SymmetricEncryptionUtils {
+
+    private static final String AES = "AES";
+    private static final String AES_CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+
+    /**
+     * This method is used to create Symmetric key of AES algorithm.
+     *
+     * @return SecretKey
+     * @throws NoSuchAlgorithmException
+     */
+    public static SecretKey createAESKey() throws NoSuchAlgorithmException {
+        SecureRandom secureRandom = new SecureRandom();
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+        keyGenerator.init(256, secureRandom);
+        return keyGenerator.generateKey();
+    }
+
+    /**
+     * This method return initialization vector.
+     *
+     * @return
+     */
+    public static byte[] createInitializationVector() {
+        byte[] iv = new byte[16];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(iv);
+        return iv;
+    }
+
+    /**
+     * This method perform AES encrytion using provided param.
+     *
+     * @param plainText
+     * @param secretKey
+     * @param initializationVector
+     * @return
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     * @throws InvalidAlgorithmParameterException
+     */
+    public static byte[] performAESEncryption(String plainText, SecretKey secretKey, byte[] initializationVector) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
+        Cipher cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
+        byte[] result = cipher.doFinal(plainText.getBytes());
+        return result;
+    }
+
+    /**
+     * This method perform decryption using provided param.
+     *
+     * @param cipherText
+     * @param secretKey
+     * @param initializationVector
+     * @return
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
+    public static String performAESDecryption(byte[] cipherText, SecretKey secretKey, byte[] initializationVector) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+        byte[] result = cipher.doFinal(cipherText);
+        return new String(result);
+    }
+
 }
--- src/test/java/com/nirbhay/security/encryption/SymmetricEncryptionUtilsTest.java
+++ src/test/java/com/nirbhay/security/encryption/SymmetricEncryptionUtilsTest.java
@@ -2,11 +2,38 @@ package com.nirbhay.security.encryption;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 class SymmetricEncryptionUtilsTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    public void testCreateAESKey() throws NoSuchAlgorithmException {
+        SecretKey secretKey = SymmetricEncryptionUtils.createAESKey();
+        //TODO check that key is not null
+        System.out.println(secretKey.getEncoded());
+    }
+
+    @Test
+    public void testPerformAESEncryptionDecryption() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
+        SecretKey key = SymmetricEncryptionUtils.createAESKey();
+        //TODO check that key is not null
+        byte[] iv = SymmetricEncryptionUtils.createInitializationVector();
+        String plainText = "This is plain text which is to be encrypted by AES algo.";
+        byte[] cipherText = SymmetricEncryptionUtils.performAESEncryption(plainText, key, iv);
+        String decryptedText = SymmetricEncryptionUtils.performAESDecryption(cipherText, key, iv);
+        //TODO check plaintext matches with decrypted text
     }
 
     @AfterEach
