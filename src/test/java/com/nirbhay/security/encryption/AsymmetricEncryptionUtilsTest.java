2f73b17fb6232c5256e26b3a2bcde31d1608a7fd
Nirbhay <da201501068@gmail.com>
2020-06-12 20:37:51 +0530

    trying to generate x.509 certificate.

--- src/test/java/com/nirbhay/security/encryption/AsymmetricEncryptionUtilsTest.java
+++ src/test/java/com/nirbhay/security/encryption/AsymmetricEncryptionUtilsTest.java
@@ -9,6 +9,7 @@ import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 
 class AsymmetricEncryptionUtilsTest {
 
@@ -16,6 +17,24 @@ class AsymmetricEncryptionUtilsTest {
     void genrateRSAKeyPair() throws NoSuchAlgorithmException {
         KeyPair keyPair = AsymmetricEncryptionUtils.genrateRSAKeyPair();
         Assert.assertNotNull(keyPair);
+    }
+
+    @Test
+    void geenrateX509Certificate() throws Exception {
+        KeyPair keyPair = AsymmetricEncryptionUtils.genrateRSAKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+//        try {
+//            CertAndKeyGen keyGen = new CertAndKeyGen("RSA", "SHA1WithRSA", null);
+//            keyGen.generate(1024);
+//
+//            //Generate self signed certificate
+//            X509Certificate[] chain = new X509Certificate[1];
+//            chain[0] = keyGen.getSelfCertificate(new X500Name("CN=ROOT"), (long) 365 * 24 * 3600);
+//
+//            System.out.println("Certificate : " + chain[0].toString());
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
     }
 
     @Test
